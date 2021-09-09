e33337032a03a14315a48fb5b2f8e427c437e92d
DDDDanny <707956456@qq.com>
2021-09-09 23:20:57 +0800

    init: 初始化项目

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from './index.less';
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>KUMI</h1>
     </div>
   );
 }
