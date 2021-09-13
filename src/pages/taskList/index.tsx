7c73a7270ae64dd42f60de88a2216e1e53bcbe8f
DDDDanny <707956456@qq.com>
2021-09-13 22:05:23 +0800

    feat: 完善任务列表页面UI结构

--- src/pages/taskList/index.tsx
+++ src/pages/taskList/index.tsx
@@ -1,7 +1,21 @@
-import { Divider, Row, Col, Table, Tag, Space } from 'antd';
 import React from 'react';
+import { SearchOutlined } from '@ant-design/icons';
+import {
+  Divider,
+  Row,
+  Col,
+  Table,
+  Tag,
+  Space,
+  Form,
+  Input,
+  Button,
+  Select,
+  DatePicker,
+} from 'antd';
 
-const style = { background: '#0092ff', padding: '8px 0' };
+const { RangePicker } = DatePicker;
+const { Option } = Select;
 
 // 测试数据
 const columns = [
@@ -14,12 +28,12 @@ const columns = [
   },
   {
     title: '任务状态',
-    dataIndex: 'taskStatus',
-    key: 'taskStatus',
+    dataIndex: 'taskState',
+    key: 'taskState',
     width: '5%',
-    render: (taskStatus: any) => (
+    render: (taskState: any) => (
       <>
-        {taskStatus.map((state: string) => {
+        {taskState.map((state: string) => {
           let color = '#0099CC';
           if (state === 'finish') {
             color = '#99CC66';
@@ -36,7 +50,7 @@ const columns = [
     ),
   },
   {
-    title: '任务进度',
+    title: '进度',
     dataIndex: 'taskProgress',
     key: 'taskProgress',
     width: '5%',
@@ -49,6 +63,7 @@ const columns = [
     key: 'action',
     render: () => (
       <Space size="middle">
+        <a>新增</a>
         <a>编辑</a>
         <a>删除</a>
       </Space>
@@ -62,8 +77,8 @@ const data = [
     key: '1',
     taskName: '任务1',
     taskContent: '前端测试1',
-    taskStatus: ['ready'],
-    taskProgress: '0%',
+    taskState: ['ready'],
+    taskProgress: 0,
     startTime: '2021.09.11',
     endTime: '2021.09.11',
     creator: 'DDDDanny',
@@ -72,8 +87,8 @@ const data = [
     key: '2',
     taskName: '任务2',
     taskContent: '前端测试2',
-    taskStatus: ['going'],
-    taskProgress: '50%',
+    taskState: ['going'],
+    taskProgress: 50,
     startTime: '2021.09.11',
     endTime: '2021.09.11',
     creator: 'DDDDanny',
@@ -82,8 +97,8 @@ const data = [
     key: '3',
     taskName: '任务3',
     taskContent: '前端测试3',
-    taskStatus: ['finish'],
-    taskProgress: '100%',
+    taskState: ['finish'],
+    taskProgress: 100,
     startTime: '2021.09.11',
     endTime: '2021.09.11',
     creator: 'DDDDanny',
@@ -96,26 +111,65 @@ export default class TaskList extends React.Component<any, any> {
     super(props);
   }
 
+  onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
   render() {
     return (
       <div>
-        <Divider orientation="left">条件搜索</Divider>
-        <Row gutter={16}>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-        </Row>
+        <Divider orientation="left">查 询</Divider>
+        <Form
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={this.onFinish}
+          onFinishFailed={this.onFinishFailed}
+          autoComplete="off"
+        >
+          <Row justify="space-around">
+            <Col span="6">
+              <Form.Item label="任务名称" name="taskName">
+                <Input placeholder="请输入任务名称" allowClear />
+              </Form.Item>
+            </Col>
+            <Col span="6">
+              <Form.Item label="任务状态" name="taskState">
+                <Select placeholder="请选择任务状态" allowClear>
+                  <Option value="finish">已完成</Option>
+                  <Option value="going">进行中</Option>
+                  <Option value="ready">未开始</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col span="6">
+              <Form.Item label="执行时间" name="taskTime">
+                <RangePicker
+                  showTime
+                  allowClear
+                  placeholder={['任务开始时间', '任务结束时间']}
+                />
+              </Form.Item>
+            </Col>
+            <Col span="1">
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  shape="round"
+                  icon={<SearchOutlined />}
+                >
+                  搜索
+                </Button>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
         <Divider orientation="left">任务表</Divider>
-        <Table dataSource={data} columns={columns} />
+        <Table dataSource={data} columns={columns} bordered />
       </div>
     );
   }
